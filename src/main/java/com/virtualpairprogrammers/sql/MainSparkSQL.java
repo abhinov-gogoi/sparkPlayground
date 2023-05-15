29966de55161bbbe1a1467e8376f6f5c368c2193
Abhinov <abhinov.in@gmail.com>
2023-05-15 08:40:07 +0530

    sparkRDD sql filters

--- src/main/java/com/virtualpairprogrammers/sql/MainSparkSQL.java
+++ src/main/java/com/virtualpairprogrammers/sql/MainSparkSQL.java
@@ -2,21 +2,22 @@ package com.virtualpairprogrammers.sql;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.*;
+import static org.apache.spark.sql.functions.*;
+
 
 public class MainSparkSQL {
     public static void main(String[] args) {
 
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
-        SparkSession sparkSession = SparkSession.builder()
+        SparkSession spark = SparkSession.builder()
                 .appName("MainSparkSQL")
                 .master("local[*]")
                 .getOrCreate();
 
-        Dataset<Row> dataset = sparkSession
+        Dataset<Row> dataset = spark
                 .read()
                 .option("header", true)
                 .csv("src/main/resources/exams/students.csv");
@@ -24,14 +25,28 @@ public class MainSparkSQL {
         long noOfRows = dataset.count();
         System.out.println(noOfRows);
 
-        Row firstRow = dataset.first();
-        System.out.println(firstRow.getAs("grade").toString());
+        // filters using SQL like expressions
+        Dataset<Row> onlyMathSubject = dataset.filter(" subject = 'Math' AND year >= 2007 ");
 
-        dataset.show();
+        // filter using java lambdas
+        Dataset<Row> onlyMathUsingLambdaFilter = dataset.filter(
+                (FilterFunction<Row>) row -> row.getAs("subject").equals("Math")
+                        && Integer.parseInt(row.getAs("year")) >= 2007);
+
+        // filters using columns todo :: data type mismatch in geq method. using $greater$eq instead
+
+        Column subjectCol = dataset.col("subject");
+        Column yearCol = dataset.col("year");
+        // todo :: EXCEPTION here even when using $greater$eq method
+//        Dataset<Row> onlyMathUsingColumnFilter = dataset
+//                .filter(subjectCol.equalTo("Math").and(yearCol).geq(2005));
+
+        // using static import  -
+        // Todo :: only this works - static column import and  $greater$eq function
+        Dataset<Row> onlyMathUsingColumnFilter = dataset.filter(
+                col("subject").equalTo("Math").and(col("year").$greater$eq(2007)));
 
 
-
-
-
+        onlyMathUsingColumnFilter.show();
     }
 }
